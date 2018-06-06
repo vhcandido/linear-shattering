6aa4f8501f0dd1b9babc066a091450a8c4df90f9
Victor Hugo Candido de Oliveira <victorhcandido@gmail.com>
2018-06-06 19:34:15 -0300

    if u-l=n-p then F2 yields the same results

--- R/paper_Flup.r
+++ R/paper_Flup.r
@@ -8,7 +8,7 @@ F1 <- function(l, u, p) {
 }
 
 F2 <- function(l, u, p) {
-	if (p > 0 && l < u) {
+	if (p > 0 && l <= u) {
 		sum(sapply(l:u, function(i, u, p) F2(i+1, u+1, p-1), u=u, p=p))
 	} else { 1 }
 }
@@ -18,23 +18,13 @@ F3 <- function(n, p) {
 	length(which(apply(g, 1, function(x) all(diff(x) > 0) )))
 }
 
+print.all <- function(n, p) {
+	cat(' F1(1, n-p+1, p):', F1(1, n-(p-1), p), '\n') # paper's original one
+	cat(' F2(1, n-p+1, p):', F2(1, n-(p-1), p), '\n') # suggested on the paper (F1 slightly changed)
+	cat(' F2(0, n-p, p):', F2(0, n-p, p), '\n') # showing that u-l must be equal to n-p
+	cat(' F3(n, p):', F3(n, p), '\n') # sum of a p-dimensional tensor's triangle
+	cat(' choose(n, p):', choose(n, p), '\n\n')
+}
 
 n = 10
-
-p = 2
-F1(1, n-(p-1), p) # paper's original one
-F2(1, n-(p-1), p) # suggested on the paper (F1 slightly changed)
-F3(n, p) # sum of a p-dimensional tensor's triangle
-choose(n, p)
-
-p = 3
-F1(1, n-(p-1), p) # paper's original one
-F2(1, n-(p-1), p) # suggested on the paper (F1 slightly changed)
-F3(n, p) # sum of a p-dimensional tensor's triangle
-choose(n, p)
-
-p = 5
-F1(1, n-(p-1), p) # paper's original one
-F2(1, n-(p-1), p) # suggested on the paper (F1 slightly changed)
-F3(n, p) # sum of a p-dimensional tensor's triangle
-choose(n, p)
+. <- sapply(0:5, function(i) { cat(' n=', n, '| p=', i, '\n'); print.all(n, i) })
