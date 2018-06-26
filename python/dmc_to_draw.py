7e9a49462a1bb86cdd6be9a10730d5fe184be82c
Victor Hugo Candido de Oliveira <victorhcandido@gmail.com>
2018-06-25 22:00:41 -0300

    Parsing coordinates of polyhedron vertices and edges to plot with R

---
+++ python/dmc_to_draw.py
@@ -0,0 +1,20 @@
+#!/usr/env python
+
+import sys
+from dmccooey import Poly
+import numpy as np
+
+if __name__ == '__main__':
+    if not len(sys.argv) == 2:
+        print(' usage:\n \t%s url' % sys.argv[0])
+        exit(1)
+
+    url = sys.argv[1]
+
+    print("-> Creating object")
+    P = Poly(url)
+    P.build()
+
+    print("-> Printing to '../draw/vertices.dat' and '../draw/edges.dat")
+    np.savetxt('../draw/vertices.dat', P.vertices)
+    np.savetxt('../draw/edges.dat', P.edges, fmt='%d')
